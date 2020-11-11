a5c29e9f266482556df3047d680a57705ed03611
Rickey <weems505@gmail.com>
2020-11-11 10:47:36 -0600

    Updated Models

--- ResumeBuilderAngular/src/app/shared/general.model.ts
+++ ResumeBuilderAngular/src/app/shared/general.model.ts
@@ -7,3 +7,52 @@ export class General {
   }
 
 }
+
+export class Summary {
+  constructor(public summary: string) {
+  }
+}
+
+export class Education{
+  constructor(public school: string, public location: string,
+              public start: Date, public end: Date,
+              public current: boolean, public degree: string) {
+  }
+}
+
+export class Experience {
+  constructor(public company: string, public location: string,
+              public start: Date, public end: Date,
+              public current: boolean, public description: string) {
+  }
+}
+
+export class Skills {
+  constructor(public description: string) {
+  }
+}
+
+export class Projects {
+  constructor(public title: string, public description: string) {
+  }
+}
+
+export class Achievements {
+  constructor(public issuer: string,
+              public name: string,
+              public issueDate: Date) {
+  }
+}
+
+export class Websites {
+  constructor(public website: string) {
+  }
+}
+
+export class Resume {
+  constructor(general: General, summary: Summary,
+              education: Education[], experience: Experience[],
+              skills: Skills, projects: Projects[],
+              achievements: Achievements[], websites: Websites) {
+  }
+}

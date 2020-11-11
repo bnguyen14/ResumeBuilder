0cb0c66f0006df0d5d86a3bf39a4f02ea365ca68
Bao <baovu88@yahoo.com>
2020-11-11 15:16:05 -0500

    creating dynamic forms

--- ResumeBuilderAngular/src/app/angularresume/angular-resume/angular-resume.component.ts
+++ ResumeBuilderAngular/src/app/angularresume/angular-resume/angular-resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import {General} from '../../shared/general.model';
 
 @Component({
@@ -7,7 +8,11 @@ import {General} from '../../shared/general.model';
   styleUrls: ['./angular-resume.component.css']
 })
 export class AngularResumeComponent implements OnInit {
-  
+
+  maximumFormList = 3;
+
+  dynamicForm : FormGroup;
+
   @Output() outputName = new EventEmitter<General>();
 
   @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
@@ -15,9 +20,51 @@ export class AngularResumeComponent implements OnInit {
   @ViewChild('emailInput', {static: false}) emailInputRef: ElementRef;
   @ViewChild('numberInput', {static: false}) numberInputRef: ElementRef;
   
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.dynamicForm = this.formBuilder.group({
+      websites: new FormArray([])
+    });
+    this.websiteFormArray.push(this.formBuilder.group({
+      website : ''
+    }));
+  }
+  //the overall form control of "dynamicForm"
+  get formControl() { return this.dynamicForm.controls; }
+  //use FormArray to push another form into the array
+  get websiteFormArray() { return this.formControl.websites as FormArray }
+  //use in .html file to find how many forms are in a group
+  get websiteFormGroup() { return this.websiteFormArray.controls as FormGroup[] }
+
+  //adds another set of the form in the specific category
+  incrementList(category:string){
+    switch(category){
+      case 'website':{
+        if(this.websiteFormGroup.length<=this.maximumFormList){
+          this.websiteFormArray.push(this.formBuilder.group({
+            website : ''
+          }));
+        }
+        break;
+      }
+    }
+  }
+
+  decrementList(category:string, i:number){
+    switch(category){
+      case 'website':{
+        if(this.websiteFormGroup.length<=this.maximumFormList){
+          this.websiteFormArray.removeAt(i);
+        }
+        break;
+      }
+    }
+  }
+
+  printResume(){
+    console.log("as value: "+this.dynamicForm.value);
+    console.log("as json: "+JSON.stringify(this.dynamicForm.value, null, 4));
   }
 
   sendName() {

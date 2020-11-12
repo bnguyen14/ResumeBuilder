3b85bbc9f30cbb425225b94a9a401a7ab888d966
Pierce Trotman <ptrotman22@gmail.com>
2020-11-12 10:53:08 -0500

    Inserted SQL Templates

---
+++ MySQL/Insert into Resume Database.sql
@@ -0,0 +1,22 @@
+INSERT INTO user VALUES("1", "ptrotman@gmail.com", "databaseCreator");
+INSERT INTO resume VALUES("1", "1", "Pierce Trotman", "ptrotman@gmail.com", "Atlanta, Georgia", 'Software Developer with experience working with Java, Hibernate and MySQL implementing methodologies of the IT industry using mediation and perseverance with a focus on problem solving and customer satisfaction.', "Software & Tools: Windows, Mac OSX, IntelliJ, MySQL Workbench, React, Spring Boot, AWS");
+INSERT INTO education VALUES("1", "1", "Georgia State University", "Atlanta, Georgia", "2014-10-01", "2018-10-01", 'Information Technology', "1");
+INSERT INTO project VALUES("1", "1", "Ticket Project", "A ticket-generating program that creates a boarding pass through a customer’s input. The program can add, delete, and update information in a comprehensive database demonstrating Java, Hibernate, MySQL, and SQL design principles.");
+INSERT INTO achievement VALUES("1", "1", "Landmark Education ", "Team Leader", "2010-10-01");
+INSERT INTO experience VALUES("1", "1", "Pyramid Academy Inc", "Alparetta, Georgia", "Java Full Stack Developer", "2020-10-03", '2020-11-12', "•	Created complex dynamic programs utilizing Java, SQL, and Hibernate.", '1');
+INSERT INTO website VALUES("1", "1", "https://github.com/ptrotman22");
+
+/*
+Data for Get,Post,Put,and Delete Commands
+Resume
+{
+	"resume_Id": "2",
+    "user_Id": "firefly34!"
+    "name": "Adam Ingram",
+    "email": "reddragon@gmail.com",
+    "location": "Redhat, Georgia",
+    "summary": "He likes coding. like at lot.",
+    "skills": "678-67-7891",
+    "email": "mcampbell42@hotmail.com"
+}
+*/

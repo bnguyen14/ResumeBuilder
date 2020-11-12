acf628589b9ccc7a4b2b341cc7f4e2235a7c373a
Rickey <weems505@gmail.com>
2020-11-12 10:04:24 -0600

    Moved resume builder

--- ResumeBuilderAngular/src/app/app.component.ts
+++ ResumeBuilderAngular/src/app/app.component.ts
@@ -21,97 +21,94 @@ export class AppComponent {
   title = 'ResumeBuilderAngular';
   something = 'test title';
 
-
-  newOutputName: {general: General, summary: Summary,
-                   education: Education[], experience: Experience[],
-                   skills: Skills, projects: Projects[],
-                   achievements: Achievements[], websites: Websites};
-
-  download(){
-    const document = new Document();
-    document.addSection({
-      children: [
-        new Paragraph({
-          text: this.something,
-          heading: HeadingLevel.HEADING_1
-        }),
-
-      ]
-    });
-
-    Packer.toBlob(document).then(blob => {
-      saveAs(blob, 'example.docx');
-    });
-  }
-
-
-  display(name) {
-    this.newOutputName = {...name};
-    console.log('name: ' + this.newOutputName.general.phoneNumber);
-    this.createNew();
-  }
-
-
-
-  createNew() {
-    const doc = new Document();
-    doc.addSection({
-      children: [
-        // general
-        new Paragraph({
-          text: this.newOutputName.general.name,
-          heading: HeadingLevel.HEADING_1
-        }),
-        new Paragraph({
-          text: this.newOutputName.general.socials,
-          heading: HeadingLevel.HEADING_2
-        }),
-        new Paragraph({
-          text: this.newOutputName.general.email,
-          heading: HeadingLevel.HEADING_2
-        }),
-        new Paragraph({
-          text: this.newOutputName.general.phoneNumber,
-          heading: HeadingLevel.HEADING_2
-        }),
-        // summary
-        new Paragraph({
-          text: this.newOutputName.summary.summary,
-          heading: HeadingLevel.HEADING_1
-        }),
-        // education
-        new Paragraph({ text: 'Education', heading: HeadingLevel.HEADING_1}),
-        new Paragraph('EDUCATION ARRAY'),
-      //  experience
-        new Paragraph({ text: 'Experience', heading: HeadingLevel.HEADING_1}),
-        new Paragraph('EXPERIENCE ARRAY'),
-      //  skills
-        new Paragraph({ text: 'Skills', heading: HeadingLevel.HEADING_1}),
-        new Paragraph({
-          text: this.newOutputName.skills.description,
-          heading: HeadingLevel.HEADING_4
-        }),
-        // projects
-        new Paragraph({ text: 'Projects', heading: HeadingLevel.HEADING_1}),
-        new Paragraph('PROJECTS ARRAY'),
-        // achievements
-        new Paragraph({ text: 'Achievements', heading: HeadingLevel.HEADING_1}),
-        new Paragraph('Achievements ARRAY'),
-        // websites
-        new Paragraph({ text: 'Websites', heading: HeadingLevel.HEADING_1}),
-        new Paragraph({
-          text: this.newOutputName.websites.website,
-          heading: HeadingLevel.HEADING_4
-        }),
-
-      ]
-    });
-
-    Packer.toBlob(doc).then((blob) => {
-      // saveAs from FileSaver will download the file
-      saveAs(blob, 'example.docx');
-    });
-
-  }
+  //
+  // newOutputName: {general: General, summary: Summary,
+  //                  education: Education[], experience: Experience[],
+  //                  skills: Skills, projects: Projects[],
+  //                  achievements: Achievements[], websites: Websites};
+  //
+  // download(){
+  //   const document = new Document();
+  //   document.addSection({
+  //     children: [
+  //       new Paragraph({
+  //         text: this.something,
+  //         heading: HeadingLevel.HEADING_1
+  //       }),
+  //
+  //     ]
+  //   });
+  //
+  //   Packer.toBlob(document).then(blob => {
+  //     saveAs(blob, 'example.docx');
+  //   });
+  // }
+  //
+  //
+  // display(name) {
+  //   this.newOutputName = {...name};
+  //   console.log('name: ' + this.newOutputName.general.phoneNumber);
+  //   this.createNew();
+  // }
+  //
+  //
+  //
+  // createNew() {
+  //   const doc = new Document();
+  //   doc.addSection({
+  //     children: [
+  //       // general
+  //       new Paragraph({
+  //         text: this.newOutputName.general.name,
+  //         heading: HeadingLevel.HEADING_1
+  //       }),
+  //       ,
+  //       new Paragraph({
+  //         text: this.newOutputName.general.email,
+  //         heading: HeadingLevel.HEADING_2
+  //       }),
+  //       new Paragraph({
+  //         text: this.newOutputName.general.phoneNumber,
+  //         heading: HeadingLevel.HEADING_2
+  //       }),
+  //       // summary
+  //       new Paragraph({
+  //         text: this.newOutputName.summary.summary,
+  //         heading: HeadingLevel.HEADING_1
+  //       }),
+  //       // education
+  //       new Paragraph({ text: 'Education', heading: HeadingLevel.HEADING_1}),
+  //       new Paragraph('EDUCATION ARRAY'),
+  //     //  experience
+  //       new Paragraph({ text: 'Experience', heading: HeadingLevel.HEADING_1}),
+  //       new Paragraph('EXPERIENCE ARRAY'),
+  //     //  skills
+  //       new Paragraph({ text: 'Skills', heading: HeadingLevel.HEADING_1}),
+  //       new Paragraph({
+  //         text: this.newOutputName.skills.description,
+  //         heading: HeadingLevel.HEADING_4
+  //       }),
+  //       // projects
+  //       new Paragraph({ text: 'Projects', heading: HeadingLevel.HEADING_1}),
+  //       new Paragraph('PROJECTS ARRAY'),
+  //       // achievements
+  //       new Paragraph({ text: 'Achievements', heading: HeadingLevel.HEADING_1}),
+  //       new Paragraph('Achievements ARRAY'),
+  //       // websites
+  //       new Paragraph({ text: 'Websites', heading: HeadingLevel.HEADING_1}),
+  //       new Paragraph({
+  //         text: this.newOutputName.websites.website,
+  //         heading: HeadingLevel.HEADING_4
+  //       }),
+  //
+  //     ]
+  //   });
+  //
+  //   Packer.toBlob(doc).then((blob) => {
+  //     // saveAs from FileSaver will download the file
+  //     saveAs(blob, 'example.docx');
+  //   });
+  //
+  // }
 
 }

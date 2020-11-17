6b8c138cd090c72baf823695e54bc227b31cf75d
Edward Cochran <edwardcochran23@gmail.com>
2020-11-17 16:59:41 -0500

    Login and Register

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AppService} from '../app.service';
+import {Router} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+loginForm: FormGroup;
+useremail: FormControl;
+userpass: FormControl;
+  constructor(private appService: AppService, public router: Router) { }
 
   ngOnInit(): void {
+    this.useremail = new FormControl('');
+    this.userpass = new FormControl('');
+    this.loginForm = new FormGroup({
+      useremail : this.useremail,
+      userpass : this.userpass
+    });
   }
-
+  loginCheck(){
+    if (this.appService.logger(this.loginForm.value.useremail, this.loginForm.value.userpass) === true)
+    {
+      this.router.navigate(['/app']);
+    }
+  }
 }
